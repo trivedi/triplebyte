93edf1730ea073bc53d8c66cb082002a0ad14cbc
Nishad Trivedi <nishad.d.trivedi@gmail.com>
2016-09-08 22:19:28 -0500

    Add content-type to header

--- request.py
+++ request.py
@@ -1,82 +1,78 @@
-class Request:
+import logging
+import os
+import util
 
+
+class Request:
+	'''
+	Object for all HTTP requests
+	'''
 	def __init__(self, filename, method):
-		self.filename = filename
+		self.filename = 'resp_files/index.html' if filename == '/' else filename
 		self.method = method
 		self.methods = {
 			'GET' : self.do_get,
 			'HEAD' : self.do_head
 		}
+		self.status = self.get_status()
 		self.data = None
+		logging.basicConfig(filename='server.log',level=logging.DEBUG,format='%(asctime)s %(message)s')
 
-	def do_request(self):
-		return self.methods.get(self.method)(self.filename)
-
-	def do_get(self, filename):
-		print filename
-		print 'DOING GET'
-
-		status = None
-
-		if filename == '/':
-			status = 200
-			filename = 'resp_codes/index.html'
-		elif os.path.exists(filename):
-			if os.state(filename).st_mode == 33188:
+	def get_status(self):
+		'''
+		Returns the HTTP status for the specified file
+		'''
+		if os.path.exists(self.filename):
+			if os.stat(self.filename).st_mode == 33188:
 				# group has read access => 200
-				status = 200
+				return 200
 			else:
 				# group does not have read access => 403
-				status = 403
+				return 403
 		else:
 			# Page doesn't exist => 404
-			status = 404
+			return 404
+
+	def do_request(self):
+		'''
+		Executes the HTTP method and returns the results as a string
+		'''
+		self.data = self.methods.get(self.method)()
+		print self.data
+		return self.data
+
+	def do_get(self):
+		'''
+		Invokes HTTP GET and returns the source code for @self.filename as a string
+		'''
+		print 'DOING GET'
 		
 		# Get the header for the requested file
-		header = self.do_head(filename, status)
+		header = self.do_head()
 
-		with open(filename, 'r') as f:
+		with open(self.filename, 'r') as f:
 			src = '\n'.join(f.readlines())
+
+		logging.info('"%s %s" %s', self.method, self.filename, self.status)
 
 		# Return the file requested (with the appropriate header)
-		return header + src
+		return header + '\r\n' + src # extra header needed to separate from body per HTTP standards (RFC 2616)
 	
-	def do_head(self, filename, status):
+	def do_head(self):
 		'''
-		Returns the header for @filename
+		Invokes HTTP HEAD and returns the header for @self.filename as a string
 		'''
-		print filename
+		print self.filename
 		print 'DOING HEAD'	
-		crlf = '\r\n\r\n'
+		crlf = '\r\n'
 
 		# Build the header
-		return 'HTTP/1.1 ' + util.resp_codes.get(status, '') + crlf
+		header = 'HTTP/1.1 ' + util.resp_codes.get(self.status, '') + crlf
+		header += 'Content-Type: ' + util.get_content_type(self.filename) + '; charset=UTF-8' + crlf
+		return header
 	
-	'''
-	# Requesting the header and source
-	if not head:
-		with open(filename, 'r') as f:
-			src = '\n'.join(f.readlines())
-		data = header + src
-
-	return data
-	'''
 
 
 
-
-def get_src(filename, status, head):
-	crlf = '\r\n\r\n'
-
-	header = 'HTTP/1.1 ' + util.resp_codes.get(status, '') + crlf
-	data = header
-	
-	# Requesting the header and source
-	if not head:
-		with open(filename, 'r') as f:
-			src = '\n'.join(f.readlines())
-		data = header + src
-
-	return data
 
 
--- util.py
+++ util.py
@@ -15,5 +15,24 @@ resp_codes = {
 	405: '405 Method Not Allowed'
 }
 
+# Content-Type
+content_type = {
+	'text' : 'text/plain',
+	'html' : 'text/html',
+	'css' : 'text/css',
+	'gif' : 'image/gif',
+    'jpg' : 'image/jpeg',
+    'jpeg' : 'image/jpeg',
+    'png': 'image/png',
+    'pdf' : 'application/pdf',
+    'mp3' : 'audio/mpeg'
+}
 
-
+def get_content_type(filename):
+	'''
+	Returns the content-type that goes in the header based on @filename's extension
+	'''
+	ext = filename[filename.rfind('.')+1:] # rfind returns the last index of specified substring
+	print filename
+	print ext
+	return content_type.get(ext, 'xxx')
