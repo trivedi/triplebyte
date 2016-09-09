f968342c6f114cc582ac6eafdca90d702bd6d8dc
Nishad Trivedi <nishad.d.trivedi@gmail.com>
2016-09-09 10:31:09 -0500

    Add more header response fields

--- request.py
+++ request.py
@@ -3,6 +3,7 @@ import logging
 import os
 import util
 import codecs
+import datetime
 
 
 class Request:
@@ -11,7 +12,7 @@ class Request:
 	'''
 	def __init__(self, filename, method, root='/website'):
 		print 'filename', filename
-		
+
 		# Routing scheme requires that everything be in absolute path format
 		if filename == '/' or filename.endswith(root):
 			self.filename = util.create_path(root, '/index.html')
@@ -27,7 +28,7 @@ class Request:
 			'HEAD' : self.do_head
 		}
 		self.status = self.get_status()
-		self.data = None
+		self.content = None
 		logging.basicConfig(filename='server.log',level=logging.DEBUG,format='%(asctime)s %(message)s')
 
 	def get_status(self):
@@ -58,33 +59,40 @@ class Request:
 		'''		
 		# Get the header for the requested file
 		header = self.do_head()
-
-		filename = self.filename
-		# Serve templated files in case there isn't an OK HTTP response
-		if self.status != 200:
-			filename = util.create_path('/resp_files', '/'+str(self.status)+'.html')
-
-		with open(filename, 'rb') as f:
-			src = f.read()
-			# src = zlib.compress(f.read())
-
 		logging.info('"%s %s" %s', self.method, self.filename, self.status)
 
 		# Return the file requested (with the appropriate header)
-		return header + '\r\n' + src # extra header needed to separate from body per HTTP standards (RFC 2616)
+		return header + self.content # extra header needed to separate from body per HTTP standards (RFC 2616)
 	
 	def do_head(self):
 		'''
 		Invokes HTTP HEAD and returns the header for @self.filename as a string
 		'''
 		crlf = '\r\n'
+		self.content = self.get_content() # save the content of in case GET is next
 
 		# Build the header
 		header = 'HTTP/1.1 ' + util.resp_codes.get(self.status, '') + crlf
 		header += 'Content-Type: ' + util.get_content_type(self.filename) + '; charset=UTF-8' + crlf
+		header += 'Content-Length: %d' % len(self.content) + crlf
+		header += 'Server: Nishad HTTP (Unix)' + crlf
+		header += 'Date: ' + datetime.datetime.now().strftime('%a, %d %b %Y %H:%M:%S CST') + crlf
 		# header += 'Content-Encoding: gzip' + crlf
-		return header
+		return header + crlf # end of header denoted by empty field
+
+	def get_content(self):
+		'''
+		Return content of @self.filename
+		'''
+		# Serve templated files in case there isn't an OK HTTP response
+		if self.status != 200:
+			self.filename = util.create_path('/resp_files', '/'+str(self.status)+'.html')
+
+		with open(self.filename, 'rb') as f:
+			src = f.read()
+			# src = zlib.compress(f.read())
 	
+		return src
 
 
 
